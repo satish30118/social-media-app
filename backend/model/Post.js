75fe79dfe1b579bd3ba226d7065f89eaec567ef5
Satish Maurya <satishmaurya30118@gmail.com>
2024-06-17 10:53:15 +0530

    post model

---
+++ backend/model/Post.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const postSchema = mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    location: String,
+    description: String,
+    picturePath: String,
+    userPicturePath: String,
+    likes: {
+      type: Map,
+      of: Boolean,
+    },
+    comments: {
+      type: Array,
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+module.exports = Post;
