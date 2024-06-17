8f31a38b1d32b0213320b26e2a4c0a6b66dd17c0
Satish Maurya <satishmaurya30118@gmail.com>
2024-06-17 16:05:43 +0530

    context api

---
+++ client/ContextApi/authContext.jsx
@@ -0,0 +1,33 @@
+import axios from "axios";
+import React, { createContext, useContext, useEffect, useState } from "react";
+const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [auth, setAuth] = useState({
+    user: null,
+    token: "I am Token",
+  });
+
+  // DEFAULTS HEADERs
+  axios.defaults.headers.common["Authorization"] = auth?.token;
+  axios.defaults.baseURL = process.env.REACT_APP_API;
+
+  useEffect(() => {
+    const data = localStorage.getItem("ACCSATDEV_USER-INFO");
+    if (data) {
+      const parsedData = JSON.parse(data);
+      setAuth({ ...auth, user: parsedData.details, token: parsedData.token });
+    }
+  }, [auth?.token]);
+
+  return (
+    <>
+      <AuthContext.Provider value={[auth, setAuth]}>
+        {children}
+      </AuthContext.Provider>
+    </>
+  );
+};
+
+const useAuth = () => useContext(AuthContext);
+export { useAuth, AuthProvider, AuthContext };
